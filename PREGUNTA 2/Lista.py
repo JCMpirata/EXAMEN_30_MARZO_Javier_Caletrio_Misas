1c46d0950f8620cbe17bc49836db837256ffbe3b
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-03-30 10:04:40 +0200

    Indice de un elento de la lista

--- PREGUNTA 2/Lista.py
+++ PREGUNTA 2/Lista.py
@@ -14,7 +14,8 @@ def imprimir_multiplos(numeros):
         if nodo.elem % 10 == 0 and nodo.elem < 200:
             print(nodo.elem)
 
-# Ordenar una lista de numeros
+
+# Ordenar una lista de numeros y parar el programa si llega a un numero mayor que 300
 def ordenar_lista(numeros):
     pila = []
     for i in numeros:
@@ -24,17 +25,39 @@ def ordenar_lista(numeros):
     lista_corta = []
 
     while pila:
+        if pila[-1].elem > 300:
+            print("Numero mayor a 300")
+            break
+
         nodo = pila.pop()
         lista_corta.append(nodo.elem)
 
     lista_corta.sort()
     return lista_corta
 
+# Devolver indice de un elemento en una lista
+def indice_elemento(lista, elemento):
+    pila = Pila()
+    for i in lista:
+        nodo = SNode(i)
+        pila.push(nodo)
+
+    indice = 0
+    while not pila.isEmpty():
+        nodo = pila.pop()
+        if nodo.elem == elemento:
+            return indice
+        indice += 1
+
+    return -1
+
+
 
 if __name__ == "__main__":
     numeros = [18, 50, 210, 80, 145, 333, 70, 30]
     print(imprimir_multiplos(numeros))
     print(ordenar_lista(numeros))
+    print(indice_elemento(numeros, 145))
 
 
 
