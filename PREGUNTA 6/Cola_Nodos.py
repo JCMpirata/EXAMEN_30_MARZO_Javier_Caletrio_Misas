03c080e0947f89f62eafebd6286881f6ba2e2c4d
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-03-30 08:56:17 +0200

    mas cola sarrus

--- PREGUNTA 6/Cola_Nodos.py
+++ PREGUNTA 6/Cola_Nodos.py
@@ -12,3 +12,26 @@ class Cola:
 
     def first(self):
         return self._head
+    
+    def enqueue(self, e):
+        newNode = SNode(e)
+        if self.isEmpty():
+            self._head = newNode
+
+        else:
+            self._tail.next = newNode
+
+        self._tail = newNode
+        self.len += 1
+
+    def dequeue(self):
+        res = None
+        if not self.isEmpty():
+            res = self._head.elem
+            self._head = self._head.next
+            if self._head is None:
+                self._tail = None
+            self.len -= 1
+        else:
+            print("Cola vacia!!!")
+        return res
