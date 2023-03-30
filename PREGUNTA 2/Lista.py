22553aab3e744bd0eeff8d8900f7f9140ac85107
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2023-03-30 09:53:00 +0200

    imprimir numeros lista

--- PREGUNTA 2/Lista.py
+++ PREGUNTA 2/Lista.py
@@ -1,6 +1,20 @@
 from Pila_Nodos import Pila
 from Pila_Nodos import SNode
 
+
+# Imprimir numeros en caso de que sea multiplo de 10 y menor que 200
+def imprimir_multiplos(numeros):
+    pila = Pila()
+    for i in numeros:
+        nodo = SNode(i)
+        pila.push(nodo)
+
+    while not pila.isEmpty():
+        nodo = pila.pop()
+        if nodo.elem % 10 == 0 and nodo.elem < 200:
+            print(nodo.elem)
+
+# Ordenar una lista de numeros
 def ordenar_lista(numeros):
     pila = []
     for i in numeros:
@@ -18,7 +32,8 @@ def ordenar_lista(numeros):
 
 
 if __name__ == "__main__":
-    numeros = [5, 2, 8, 1, 3, 9]
+    numeros = [18, 50, 210, 80, 145, 333, 70, 30]
+    print(imprimir_multiplos(numeros))
     print(ordenar_lista(numeros))
 
 
